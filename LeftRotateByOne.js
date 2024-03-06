f5805b9b5112e708e13cf18306b2050a2a206dec
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-07 01:39:09 +0530

    Efficient Method

--- LeftRotateByOne.js
+++ LeftRotateByOne.js
@@ -1,3 +1,18 @@
 function leftRotateByOne(arr) {
+
+    const num1 = arr[0]
+
+    for (let i = 1; i < arr.length; i++) {
+        //const temp = arr[i]
+        arr[i-1] = arr[i]     
+    }
+
+    arr[arr.length-1]=num1
     
 }
+
+const arr = [1,2,3,4,5]
+leftRotateByOne(arr)
+
+console.log(arr);
+
