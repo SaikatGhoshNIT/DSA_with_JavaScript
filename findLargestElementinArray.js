9c888fb7e73a86f2830a24e7895a5edbf29e8679
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-06 19:49:03 +0530

    precheck

--- findLargestElementinArray.js
+++ findLargestElementinArray.js
@@ -1,12 +1,20 @@
 function findLargest(n) {
+    //pre check.
+    if (arr.length === 0) {
+        return -1; // Return -1 for an empty array
+      }
+
     let biggest = n[0]
+    let index
 
     for(let i = 0; i<n.length;i++){
         if(biggest < n[i]){
             biggest = n[i]
+            index = i
         }
     }
-    return biggest
+    //return n.indexOf(biggest)
+    return index
 }
 
-console.log(findLargest([10,20,50,40,30]));
+console.log(findLargest([10,20,50,60,30]));
