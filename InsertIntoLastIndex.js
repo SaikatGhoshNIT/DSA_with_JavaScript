e6a46d5cf2561f8f9cc4047d4e1c5c2e0243221d
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-14 13:03:34 +0530

    done

--- InsertIntoLastIndex.js
+++ InsertIntoLastIndex.js
@@ -1,5 +1,14 @@
+const arr = [1,2,3,4,5]
+let sizeOfArray = 6
+let element = 60
+
+
 function insertAtEnd(arr,sizeOfArray,element)
 {
     // code here
-    
+    arr[sizeOfArray-1] = element
 }
+
+insertAtEnd(arr,sizeOfArray,element)
+
+console.log(arr);
